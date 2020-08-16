0939af927cc36cb7deabe79b6f76a85181263fe5
Mayk Santos <mayk012santos@gmail.com>
2020-08-15 21:26:17 -0300

    Mais um coment

--- ex040.py
+++ ex040.py
@@ -1,6 +1,7 @@
 from time import sleep
 a = 'APROVAÇÃO DE ALUNO:'
 print('{:^85}'.format('APROVAÇÃO DE ALUNO:'))
+print('Olá!')
 
 n1 = float(input('DIGITE SUA PRIMEIRA NOTA: '))
 n2 = float(input('DIGITE SUA SEGUNDA NOTA: '))
